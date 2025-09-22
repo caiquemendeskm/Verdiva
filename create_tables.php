5e83d01bcc6e4f892c9545a9cee5cb3aff610466
caique <caiquemendes507@gmail.com>
2025-09-22 09:04:18 -0300

    Delete create_tables.php

--- create_tables.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/config/database.php';
-$database = new Database();
-$db = $database->getConnection();
-$database->createTables();
-echo "Tabelas criadas e dados de exemplo inseridos com sucesso!";
-?>
-
